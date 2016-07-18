2be2ac9e1e366058f9cc6d94ac763d674cd99dbf
Regencyop <GitHub@regencyop.com>
2016-07-18 13:51:54 -0400

    Update app.js

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,5 @@
 'use strict';
 
 var four51 = {};
-four51.app = angular.module('451order', ['ngResource', 'ngRoute', 'ngAnimate', 'ngSanitize', 'ngCookies', 'ngTouch', 'ui.validate', 'ui.mask', 'headroom', 'ui.bootstrap', 'angulartics', 'angulartics.google.analytics', 'ngAutocomplete', 'OrderCloud-BackToTop', 'OrderCloud-AddToCartPreview', 'OrderCloud-SpecForms']);
+four51.app = angular.module('451order', ['ngResource', 'ngRoute', 'ngAnimate', 'ngSanitize', 'ngCookies', 'ngTouch', 'ui.validate', 'ui.mask', 'headroom', 'ui.bootstrap', 'angulartics', 'angulartics.google.analytics', 'ngAutocomplete', 'OrderCloud-BackToTop', 'OrderCloud-AddToCartPreview', 'OrderCloud-ProductMatrix', 'OrderCloud-ngFader', 'OrderCloud-SpecForms']);
+
